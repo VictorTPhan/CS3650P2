0de20cb8c3fc26c5d3edfc00d4464c758a4c8567
VictorTPhan <71913548+VictorTPhan@users.noreply.github.com>
2023-11-07 15:09:20 -0800

    Documented users package

--- project/users/User.java
+++ project/users/User.java
@@ -1,66 +1,86 @@
 package project.users;
 
-import java.util.*;
-
 import project.groups.GroupUID;
 import project.tweets.ITweet;
 import project.tweets.Tweet;
 import project.ui.UserView;
 import visitor_pattern.Entity;
 import visitor_pattern.Visitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import observer_pattern.Listener;
 import observer_pattern.Subject;
 
+/**
+ * A representation of a User within the Twitter database, that can make their
+ * own tweets and follow other users.
+ */
 public class User implements Entity, Subject, Listener {
     private String username;
     private UserUID uid;
-    private GroupUID joinedGroup;
-    private List<UserUID> followers;
-    private List<Listener> followListeners;
+    private List<Listener> followers;
     private List<UserUID> followings;
-    private List<ITweet> newsFeed;
     private List<ITweet> tweets;
-    private UserView associatedUserView = null;
+    private UserView associatedUserView;
 
+    /**
+     * Initializes all the fields of a User object.
+     * 
+     * @param username The username of this User as a String.
+     */
     public User(String username) {
         this.username = username;
         this.uid = new UserUID(this);
-        joinedGroup = null;
-        this.followers = new ArrayList<>();
         this.followings = new ArrayList<>();
-        this.newsFeed = new ArrayList<>();
         this.tweets = new ArrayList<>();
-        this.followListeners = new ArrayList<>();
+        this.followers = new ArrayList<>();
+        associatedUserView = null;
     }
 
+    /**
+     * Gets the username of this User.
+     * 
+     * @return The username of this user, as a String.
+     */
     public String getUsername() {
         return username;
     }
 
-    public GroupUID getJoinedGroup() {
-        return joinedGroup;
-    }
-
+    /**
+     * Gets the UID of this User.
+     * 
+     * @return The UserUID of this user, as a String.
+     */
     public UserUID getUID() {
         return uid;
     }
 
-    public List<UserUID> getFollowers() {
-        return followers;
-    }
-
+    /**
+     * Gets the list of accounts this User is following.
+     * 
+     * @return The list of user this User is following, as an ArrayList of UserUIDs.
+     */
     public List<UserUID> getFollowings() {
         return followings;
     }
 
-    public List<ITweet> getNewsFeed() {
-        return newsFeed;
-    }
-
+    /**
+     * Gets the list of tweets this user has made.
+     * 
+     * @return The list of tweets this user has made, as an ArrayList of ITweets.
+     */
     public List<ITweet> getTweets() {
         return tweets;
     }
 
+    /**
+     * Creates a Tweet object and notifies all followers of a new Tweet.
+     * 
+     * @param content The textual portion of the Tweet itself.
+     * @return The created Tweet.
+     */
     public Tweet tweet(String content) {
         Tweet tweet = new Tweet(content, uid);
         tweets.add(tweet);
@@ -68,17 +88,15 @@ public class User implements Entity, Subject, Listener {
         return tweet;
     }
 
+    /**
+     * Puts this User on the other User's follower's list so that this User may be
+     * notified of any new tweets from the other User.
+     * 
+     * @param other The User to follow.
+     */
     public void followUser(UserUID other) {
         this.followings.add(other);
-        other.getUser().followers.add(uid);
         listenTo(other.getUser());
-        System.out.println("Followed user");
-    }
-
-    public void joinGroup(GroupUID group) {
-        if (group.getGroup().addMember(uid)) {
-            joinedGroup = group;
-        }
     }
 
     @Override
@@ -86,16 +104,23 @@ public class User implements Entity, Subject, Listener {
         visitor.visit(this);
     }
 
+    /**
+     * Links this User object to a JSwing Frame so that the frame can be refreshed
+     * whenever this User is notified of any new data to display.
+     * 
+     * @param newUserView The UserView object to associate with this User.
+     */
     public void linkToNewView(UserView newUserView) {
         if (newUserView != null) {
             this.associatedUserView = newUserView;
-            System.out.println("Connected to user view");
         }
     }
 
+    /**
+     * A function that tells the UserView to refresh its content.
+     */
     private void notifyUserView() {
         if (associatedUserView != null) {
-            System.out.println("Notified User View");
             associatedUserView.refresh();
         }
     }
@@ -117,17 +142,17 @@ public class User implements Entity, Subject, Listener {
 
     @Override
     public void register(Listener l) {
-        followListeners.add(l);
+        followers.add(l);
     }
 
     @Override
     public void deregister(Listener l) {
-        followListeners.remove(l);
+        followers.remove(l);
     }
 
     @Override
     public void notifyListeners() {
-        for (Listener l : followListeners) {
+        for (Listener l : followers) {
             l.update();
         }
     }
--- project/users/UserUID.java
+++ project/users/UserUID.java
@@ -2,20 +2,39 @@ package project.users;
 
 import java.util.UUID;
 
+/**
+ * A class that identifies a User object.
+ */
 public class UserUID {
     private User associatedUser;
     private String UID;
 
+    /**
+     * Associates a newly instantiated UID with a particular User object and
+     * generates a UID for that user.
+     * 
+     * @param associatedUser The User to associate this UID with.
+     */
     public UserUID(User associatedUser) {
         this.associatedUser = associatedUser;
         this.UID = UUID.randomUUID().toString();
         System.out.println(this.UID);
     }
 
+    /**
+     * Accesses the user associated with this UID.
+     * 
+     * @return The User associated with this UID.
+     */
     public User getUser() {
         return associatedUser;
     }
 
+    /**
+     * Accesses the UID String associated with this UID.
+     * 
+     * @return The UID String associated with this UID.
+     */
     public String getUID() {
         return UID;
     }
